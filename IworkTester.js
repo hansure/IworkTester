c8f5673a142a5f49863a3623811473eed29648be
Surafel Melese Tebeje <58382595+hansure@users.noreply.github.com>
2022-01-28 09:43:50 -0500

    Update IworkTester.js

--- IworkTester.js
+++ IworkTester.js
@@ -6,7 +6,7 @@ async function testerFun() {
 
   //Navigate to our application
   await driver.get('https://www.iworkplc.com/contact')
-  // Get element with tag name 'div'
+  // Get element with id 'comp-kktfmgn8'
   let officeNumber = driver.findElement(By.id('comp-kktfmgn8'))
   let officeNumbers = await officeNumber.findElements(By.css('p'))
   for (let e of officeNumbers) {
@@ -15,25 +15,25 @@ async function testerFun() {
   var lastFourOfficeNumberDigit =
     parseInt(officeNumberResult.replace(/[^\d\.\-]/g, '')) % 10000
   //Displaying Tele Phone values
-  console.log(lastFourOfficeNumberDigit)
+  console.log('Office Number:', lastFourOfficeNumberDigit)
 
   //For the mobile phone
-  // Get element with tag name 'div'
+  // Get element with id 'comp-ks7htvty'
   let mobileNumber = driver.findElement(By.id('comp-ks7htvty'))
   let mobileNumbers = await mobileNumber.findElements(By.css('p'))
   for (let e of mobileNumbers) {
     var mobileNumberResult = await e.getText()
   }
   var lastFourMobileNumberDigit =
-    parseInt(mobileNumberResult.replace(/[^\d\.\-]/g, '')) % 10000
+    parseInt(mobileNumberResult.replace(/\D/g, '')) % 10000
 
   //Displaying mobile phone
-  console.log(lastFourMobileNumberDigit)
+  console.log('Mobile Number:', lastFourMobileNumberDigit)
 
   //Divding last four digit of tele phone number by last four digit of mobile phone number
   var result = lastFourOfficeNumberDigit / lastFourMobileNumberDigit
   //Displaying the divided result
-  console.log(result)
+  console.log('Result: ', result)
   //Close
   await driver.quit()
 }
